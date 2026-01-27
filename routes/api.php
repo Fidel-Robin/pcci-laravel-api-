e18aafcb18604b5e4420b5044d2bb348f19085bf
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-01-28 03:17:18 +0800

    API set up

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/test', function() {
+    return response()->json([
+        'status' => 'API working',
+        'time' => now()
+    ]);
+});
