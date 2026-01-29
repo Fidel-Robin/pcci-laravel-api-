9698bc18dc85b89bfc0b1241937116a12f422741
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-01-30 00:14:05 +0800

    migrated'

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\PostController as PostController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +16,20 @@ Route::get('/test', function() {
         'time' => now()
     ]);
 });
+
+Route::get('/hello', function(){
+    return ["message" => "Hello, World!"];
+});
+
+// Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+// Route::post('/posts', [PostController::class, 'store'])->name('post.store');
+// Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
+
+
+Route::prefix('v1')->group(function(){
+    Route::apiResource('posts', PostController::class);
+});
+
+
+
+
