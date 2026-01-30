9ff3c3ff91a46afedca386f0c594c92f43df60ee
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-01-30 15:30:38 +0800

    create Post CRUD with Resources

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
@@ -12,25 +14,16 @@ class PostController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        return Post::all();
-
-        // return [
-        //     'id'=> 21,
-        //     'title'=> "title test",
-        //     'body'=>"body testss"
-        // ];
+    {   
+        return PostResource::collection(Post::with('author')->paginate());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        $data = $request ->validate([
-            'title' => 'required|string|min:2',
-            'body' => ['required', 'string', 'min:2']
-        ]);
+        $data = $request ->validated();
 
         // return $data;
 
@@ -38,39 +31,40 @@ class PostController extends Controller
 
         $post = Post::create($data);
 
-        return response()->json($post, 201);
+        // return response()->json(new PostResource($post), 201);
+        return new PostResource($post);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)  //this will automatically fetchc the post by id
     {
-        return response() -> json([
-            'id'=> 1,
-            'title'=> "Test",
-            'body'=>"post body"
-        ]);
+        // return response()->json(new PostResource($post));
+        return new PostResource($post);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post) //this will automatically fetchc the post by id
     {
         $data = $request -> validate([
             'title' => 'required|string|min:2',
             'body' => ['required', 'string', 'min:2']
         ]);
 
-        return $data;
+        $post -> update($data);
+        // return response()->json(new PostResource($post));
+        return new PostResource($post);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post) ////this will automatically fetchc the post by id
     {
-        return response() ->noContent();
+        $post->delete();
+        return response()->noContent();
     }
 }
---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|min:4',
+            'body' => ['required', 'string', 'min:2'],
+            // 'tags' => 'array',
+            // 'tags.*' => 'string|min:2'
+        ];
+    }
+
+    public function messages()
+    {
+        return[
+            'title.required' => 'title is required',
+            'title.string' => 'title must be valid string',
+            'title.min' => 'title must be at least :min characters long'
+        ];
+    }
+}
