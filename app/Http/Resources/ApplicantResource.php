e9eab30ae09a73d78ff8feafc182eb977de5192f
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-02-26 12:54:06 +0800

    fix Get Member App route

--- app/Http/Resources/ApplicantResource.php
+++ app/Http/Resources/ApplicantResource.php
@@ -146,26 +146,83 @@ class ApplicantResource extends JsonResource
         // MEMBER (NEW)
         if ($user->hasRole('member')) {
             return [
-                'id' => $this->id,
-                'status' => $this->status,
-                'date_submitted' => $this->date_submitted?->toDateString(),
+                 // === FOR PCCI USE ONLY (Upper Right) ===
+                'date_submitted'  => $this->date_submitted?->toDateString(),
+                'status'          => $this->status,
+                'date_approved'   => $this->date_approved?->toDateString(),
                 'membership_type' => $this->membership_type,
+
+                // === PHOTO ===
+                'photo_url' => $this->photo_path ? asset('storage/' . $this->photo_path) : null,
+
+                // === DOCUMENTS ===
+                'mayors_permit_url' => $this->mayors_permit_path
+                    ? route('applicants.download', ['applicant' => $this->id, 'type' => 'mayors_permit'])
+                    : null,
+
+                'dti_sec_url' => $this->dti_sec_path
+                    ? route('applicants.download', ['applicant' => $this->id, 'type' => 'dti_sec'])
+                    : null,
+
+                // === BASIC PROFILE ===
                 'basic_profile' => [
                     'registered_business_name' => $this->registered_business_name,
                     'trade_name'               => $this->trade_name,
-                    'email'                    => $this->email,
-                    'business_address'         => $this->business_address,
-                    'city_municipality'        => $this->city_municipality,
-                    'province'                 => $this->province,
-                    'region'                   => $this->region,
+
+                    'business_location' => [
+                        'business_address'  => $this->business_address,
+                        'city_municipality' => $this->city_municipality,
+                        'province'          => $this->province,
+                        'region'            => $this->region,
+                        'zip_code'          => $this->zip_code,
+                    ],
+
+                    'telephone_no' => $this->telephone_no,
+                    'website'      => $this->website_socmed,
+                    'member_dob'   => $this->member_dob?->toDateString(),
+                    'email'        => $this->email,
+                    'tin_no'       => $this->tin_no,
                 ],
+
+                // === OFFICIAL REPRESENTATIVE TO PCCI ===
                 'official_representative' => [
                     'first_name'  => $this->rep_first_name,
+                    'mid_name'    => $this->rep_mid_name,
                     'surname'     => $this->rep_surname,
                     'designation' => $this->rep_designation,
+                    'dob'         => $this->rep_dob?->toDateString(),
                     'contact_no'  => $this->rep_contact_no,
                 ],
-                'photo_url' => $this->photo_path ? asset('storage/' . $this->photo_path) : null,
+
+                // === ALTERNATE REPRESENTATIVE ===
+                'alternate_representative' => [
+                    'first_name'  => $this->alt_first_name,
+                    'mid_name'    => $this->alt_mid_name,
+                    'surname'     => $this->alt_surname,
+                    'designation' => $this->alt_designation,
+                    'dob'         => $this->alt_dob?->toDateString(),
+                    'contact_no'  => $this->alt_contact_no,
+                ],
+
+                // === MEMBERSHIP IN OTHER BUSINESS ORGANIZATION ===
+                'organization_membership' => [
+                    'name_of_organization' => $this->name_of_organization,
+                    'registration_number'  => $this->registration_number,
+                    'date_of_registration' => $this->date_of_registration?->toDateString(),
+                    'type_of_company'      => $this->type_of_company,
+                    'number_of_employees'  => $this->number_of_employees,
+                    'year_established'     => $this->year_established,
+                ],
+
+                // === PCCI-VALENZUELA CITY USE ONLY ===
+                'internal_tracking' => [
+                    'recommending_approval' => $this->recommending_approval,
+                ],
+
+                'timestamps' => [
+                    'created_at' => $this->created_at?->toDateTimeString(),
+                    'updated_at' => $this->updated_at?->toDateTimeString(),
+                ],
             ];
         }
 
